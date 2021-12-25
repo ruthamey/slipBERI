280876bc4d53bd34e73819d31655eecfc5cce5d4
Bryan Marfito <bjmarfito@yahoo.com>
2021-12-25 14:26:22 +0800

    Uncommented correct rake calculation

--- slipBERImbin/convert_fault_from_GBIS_for_slipBERI.m
+++ slipBERImbin/convert_fault_from_GBIS_for_slipBERI.m
@@ -16,8 +16,8 @@ strikeslip = optimalmodel(8);
 dipslip = optimalmodel(9);
 
 % Calculate rake
-%rake = tand(dipslip/strikeslip);
-rake =90;
+rake = tand(dipslip/strikeslip);
+%rake =90;
 
 % Convert x and y from local coordinate system to lat long
 load(inversion_result_file, 'geo')
